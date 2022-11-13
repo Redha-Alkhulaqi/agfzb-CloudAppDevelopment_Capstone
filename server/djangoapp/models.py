8ad6fdf1ae2c16e9f30f0144ae6db006ca000092
Redha-Alkhulaqi <81948469+Redha-Alkhulaqi@users.noreply.github.com>
2022-11-13 16:52:45 +0300

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -8,10 +8,7 @@ import json
 # Create your models here.
 
 # <HINT> Create a Car Make model `class CarMake(models.Model)`:
-# - Name
-# - Description
-# - Any other fields you would like to include in car make model
-# - __str__ method to print a car make object
+
 class CarMake(models.Model):
     name = models.CharField(null=False, max_length=50)
     description = models.CharField(null=True, max_length=1000)
@@ -21,13 +18,7 @@ class CarMake(models.Model):
 
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
-# - Many-To-One relationship to Car Make model (One Car Make has many Car Models, using ForeignKey field)
-# - Name
-# - Dealer id, used to refer a dealer created in cloudant database
-# - Type (CharField with a choices argument to provide limited choices such as Sedan, SUV, WAGON, etc.)
-# - Year (DateField)
-# - Any other fields you would like to include in car model
-# - __str__ method to print a car make object
+
 
 class CarModel(models.Model):
     make = models.ForeignKey(CarMake, null=True, on_delete=models.CASCADE) 
